429b264f112ef2326f503cfa6a699a67e98075b9
AdilMir1433 <adil.mir@devsinc.com>
2024-06-06 19:05:10 +0500

    fix/id validator added

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { Router } from 'express';
 import {
   createPostHandler,
@@ -6,10 +7,42 @@ import {
   updatePostHandler,
   deletePostHandler,
 } from '@src/controllers/post.controller';
+import { validateFields } from '@src/middleware/validation.middleware';
+import { body, param } from 'express-validator';
+import { User } from '@src/models/user.model';
+import { Post } from '@src/models/post.model';
 
 const router = Router();
 
-router.post('/', createPostHandler);
+export const createPostValidationRules = [
+  body('title').notEmpty().withMessage('Title is required'),
+  body('content').notEmpty().withMessage('Content is required'),
+  body('userId')
+    .notEmpty()
+    .withMessage('User ID is required')
+    .custom(async (userId) => {
+      const user = await User.findById(userId);
+      if (!user) {
+        throw new Error('User ID does not exist');
+      }
+      return true;
+    }),
+];
+
+export const idValidationRules = [
+  param('id')
+    .isMongoId()
+    .withMessage('Invalid ID format')
+    .custom(async (id) => {
+      const post = await Post.findById(id);
+      if (!post) {
+        throw new Error('Post ID does not exist');
+      }
+      return true;
+    }),
+];
+
+router.post('/', createPostValidationRules, validateFields, createPostHandler);
 router.get('/', getAllPostsHandler);
 router.get('/:id', getPostByIdHandler);
 router.put('/:id', updatePostHandler);
